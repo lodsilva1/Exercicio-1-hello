87e3dca4c30bc538513e587637bca76ad25de080
Luiz Fernando Fernando <>
2022-12-02 01:01:31 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+  //declara variavel
+  //var name : caractere
+string nome;
+    
+  //leia(nome)
+nome = Console.ReadLine();
+
+  //Escreval("Hello world")  
+    Console.WriteLine ("Hello " + nome);
+  }
+}
